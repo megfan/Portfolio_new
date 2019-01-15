039d8eb9b3ca552b169c67a061d8fcff176784c3
megfan <megfa@yahoo.com>
2019-01-15 12:57:50 +0100

    update

---
+++ README.md
@@ -0,0 +1,19 @@
+# Personal Portfolio
+Minimal One Pager as short presentation of latest projects.
+
+## View:
+[Portfolio](https://megfan.github.io)
+
+## Built With
+
+* Html5
+* CSS
+* Java Script
+* jQuery
+* AOS - Animate on scroll library
+
+### RPD
+
+### Authors
+
+* **Margaret Staszewska**
--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//cursor_______
 $(window).mousemove(function(e) {
     $('.cursor')
       .eq(0)
@@ -24,10 +25,8 @@ $(window).on('load', function() {
             console.log("sdsdsdsdsdth");
             $("#loader_sec").addClass("fade-up");
             $(document).scrollTop(0);
-        }, 3000);
+        }, 2000);
 });
-
-//cursor_______
 
 //scrolllll_______________
 
@@ -81,53 +80,4 @@ function handle(delta) {
 
 if (window.addEventListener) {window.addEventListener('DOMMouseScroll', wheel, false);}
   window.onmousewheel = document.onmousewheel = wheel;
-
-
-
-
-
-
-
-
-
-
-
-// $(".project").hover3d({
-//     selector: ".project__card",
-//     shine: false,
-//   });
-  
-//   $(".project").hover3d({
-
-//     // selector for element
-//     selector      : null,
-  
-//     // Perspective value for 3d space
-//     perspective   : 10000,
-  
-//     // Mouse movement sensitivity
-//     sensitivity   : 10,
-  
-//     // Default behavior is the element will follow the mouse, look like it facing the mouse
-//     invert        : false,
-  
-//     // Add shining layer
-//     shine       : false,
-  
-//     // Helper class when mouse hover in the element
-//     hoverInClass  : "hover-in",
-  
-//     // Helper class when mouse hover Out the element
-//     hoverOutClass : "hover-out",
-  
-//     // Helper class when the mouse is hovering the element
-//     hoverClass    : "hover-3d"
-    
-//   });
-
-//   var listMenu = document.querySelectorAll('.list_item');
-
-//   listMenu[0].addEventListener('click', function(){
-//     window.scrollTo(300, 500); 
-//   })
 
